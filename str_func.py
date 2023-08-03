aa5ffc026d5f43713aa7643a6f9e9bb4f78c3073
Dmitriy Taraskin <dmit.taraskin@gmail.com>
2023-08-03 16:44:13 +0500

    function add new func

--- str_func.py
+++ str_func.py
@@ -1,6 +1,13 @@
 def add(a):
+    """Выводит слово в верхний регистр"""
     a.upper()
+    return a
+
+def add2(a):
+    """Выводит слово в верхний регистр"""
+    a.titel()
     return a
 
 
 print(add('python'))
+print(add2('hello python'))
